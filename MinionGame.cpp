872e17a6c89bb34596cfff34af60d9b4ffbc0fa7
dowdje <dowdje89@gmail.com>
2016-06-28 21:38:27 -0500

    Update MinionGame.cpp

--- MinionGame.cpp
+++ MinionGame.cpp
@@ -3,109 +3,14 @@
 #include <cstdlib>
 #include <fstream>
 using namespace std;
-const int inventory_array_size = 3; // Initializes a maximum inventory size.
+
+const int inventory_array_size = 3; // Initializes the constant for maximum inventory size.
 string inventory_array[inventory_array_size] = {"empty", "empty", "empty"}; // Initializes an inventory array.
-void buff() // Reads buff spell to the screen.
-{
-cout << "******************************************\n"
-     << "You are endowed with the POWER OF THE GODS!"
-     << "\n****************************************\n";
-     return;
-}
-//********************************************
-//FIRST TREASURE FUNCTION
-//********************************************
-string item_drop1 () // A function that generates drops from low level enemies.
-{
-    int drop_roll,
-        drop;
-    const int ARRAY_ITEM_SIZE = 6;
-    string array_item [ARRAY_ITEM_SIZE] = {"Nothing", "Lifeblood", "Corrupted Soul", // Populates an array of possible items
-     "Knuckle Spikes", "Glittering Emerald Ring", "Gilded Behemoth Spine"};
 
-    srand(time(NULL));
-    drop_roll = (rand() % 105 + 1); // assigns proabilities for each potential item to be dropped and rolls for that item.
+void buff(); //Displays buff spell to the screen
 
-    if (drop_roll <= 70)
-        {
-            array_item [0];
-            drop = 0;
-        }
-    else if (drop_roll <= 85)
-        {
-            array_item [1];
-            drop = 1;
-        }
-    else if (drop_roll <= 94)
-        {
-            array_item [2];
-            drop = 2;
-        }
-    else if (drop_roll <= 100)
-        {
-            array_item [3];
-            drop = 3;
-        }
-    else if (drop_roll <= 103)
-        {
-            array_item [4];
-            drop = 4;
-        }
-    else if (drop_roll <= 105)
-        {
-            array_item [5];
-            drop = 5;
-        }
-
-    return array_item [drop];
-}
-//********************************************
-//SECOND TREASURE FUNCTION
-//********************************************
-string item_drop2 ()// A function that determines items to be dropped for high level enemies.
-{
-    int drop_roll, //Variable of the integer value rolled.
-        drop; // variable of which item gets called from the array
-    const int ARRAY_ITEM_SIZE = 6;
-    string array_item [ARRAY_ITEM_SIZE] = {"Nothing", "Lifeblood", "Corrupted Soul",// Populates an array of potential items
-     "Knuckle Spikes", "Glittering Emerald Ring", "Gilded Behemoth Spine"};
-
-    srand(time(NULL));
-    drop_roll = (rand() % 105 + 1);
-
-    if (drop_roll <= 25)
-        {
-            array_item [0];
-            drop = 0;
-        }
-    else if (drop_roll <= 50)
-        {
-            array_item [1];
-            drop = 1;
-        }
-    else if (drop_roll <= 75)
-        {
-            array_item [2];
-            drop = 2;
-        }
-    else if (drop_roll <= 90)
-        {
-            array_item [3];
-            drop = 3;
-        }
-    else if (drop_roll <= 100)
-        {
-            array_item [4];
-            drop = 4;
-        }
-    else if (drop_roll <= 105)
-        {
-            array_item [5];
-            drop = 5;
-        }
-
-    return array_item [drop];
-}
+string item_drop1 (); // A function that generates drops from low level enemies.
+string item_drop2 ();// A function that determines items to be dropped for high level enemies.
 
 int main()
 {
@@ -130,11 +35,10 @@ int main()
         hero_atk_mod = 0,// A modifier that gets added to the damage a player does
         attack_roll,// A roll that determines the base attack of a player action
         drop_roll,//A roll that determines the drop after an enemy dies
-        defense = 0,
-        flame,
-        frost,
-        monster_health,
-        frost_dmg;
+        flame,// flame spell
+        frost,// frost spell
+        monster_health,// current monster health
+        frost_dmg;// damage done by frost spell
 
     string action,
            drop,
@@ -146,13 +50,9 @@ int main()
 
     do
     {
-
         do
         {
-
            monster = rand() % 30 + ((21 + (5 * level))/2);
-
-
 //*********************************
 //                GENERATE MONSTER
 //*********************************
@@ -202,7 +102,7 @@ int main()
 //                      MONSTER FIGHT
 //*********************************************************
 
-        monster_atk_mod += (1 + level * level)/2;
+        monster_atk_mod += (1 + level * level)/2;// determines monst attak modifier for current encounter
 
         while (monster_health > 0)
         {
@@ -212,8 +112,8 @@ int main()
               cout << monster_name << " lashes out in fury swinging for " << monster_damage << " damage!\n\n";
               life -= monster_damage;
 
-              if (life <= 0 )
-              {
+              if (life <= 0 )//Displays player death before terminating program
+              { 
                   cout << "You grab your chest in pain and fall to the ground. You are DEAD.........................\n\n";
 
                   system("PAUSE>NUL");
@@ -225,12 +125,8 @@ int main()
             }
             cin >> action;
             cout << endl;
-// *******************
-//temporary or statements: will create function
 
-
-
-           if (action == "help")
+           if (action == "help")// displays a list of possible command inputs 
            {
                cout << "\n" << "COMMAND LIST: " << endl
                     << "\n" << "ENTER 'attack' to strike the monster.\n"
@@ -241,13 +137,13 @@ int main()
                     << "ENTER 'flame' to cast a fire spell that blasts your enemies.\n"
                     << "ENTER 'inv' to check the contents of your inventory.\n\n";
            }
-           else if (action == "inv")
+           else if (action == "inv")// displays player inventory
            {
                cout << "Weapon: " << inventory_array [0] << endl
                     << "Armor: " << inventory_array [1] << endl
                     << "Miscellaneous: " << inventory_array [2] << endl << endl;
            }
-           else if ( action == "stats")
+           else if ( action == "stats")// displays player's current stats
            {
                cout << "You are level " << level << "." << endl
                     << "You currently have " << life << " health and " << intelligence << " intelligence." << endl
@@ -256,43 +152,43 @@ int main()
                     << "You have " << exp_total << " experience and need " << (1000 * level * level) - exp_total << " to level up.\n\n";
 
            }
-            else if (action == "a" || action == "A" || action == "atk" || action == "attack")
+            else if (action == "a" || action == "A" || action == "atk" || action == "attack")//performs a player attack
            {
-               attack_roll = rand() % 20 + 1;
-               attack = attack_roll + hero_atk_mod;
-               if ( attack_roll <= 2 )
+               attack_roll = rand() % 20 + 1;// base attack roll
+               attack = attack_roll + hero_atk_mod;// adds player's attack mod to attack roll
+               if ( attack_roll <= 2 )// performs a missed attack and displays it
                     {
                         // YOU MISSED!
                         cout << "Your attack misses.\n";
                         ++monster_attack_counter;
                     }
-                    else if ( attack_roll == 20)
+                    else if ( attack_roll == 20)// checks attack roll for an aut-kill
                     {
-                        cout << "You crush the " << monster_name << " with SPLENDID FORCE!\n\n";
-                        monster_health = 0;
+                        cout << "You crush the " << monster_name << " with SPLENDID FORCE!\n\n";// displays auto-kill
+                        monster_health = 0;// performs auto-kill
                     }
-                    else
+                    else//performs a regular attack
                     {
                         // HIT THE monster
-                        monster_health -= attack;
+                        monster_health -= attack;// damages minion for damage equalt to player's current attack
 
-                        cout << "You hit the monster for " << attack
+                        cout << "You hit the monster for " << attack//displays damage
                          << " damage!\n";
-                         ++monster_attack_counter;
+                         ++monster_attack_counter;// increments monster attack readiness
                     }
            }
 //*********************************************************
 //                      SPELLS
 //*********************************************************
-           else if (action == "h" || action == "H" || action == "heal")
+           else if (action == "h" || action == "H" || action == "heal")// performs a heal spell
            {
-                     if (spell_counter > 0)
+                     if (spell_counter > 0)//checks spell well for sufficient spell orbs
                      {
-                        heal = intelligence * 10;
-                        cout << "You heal for " << heal << " health.";
-                        life += heal;
-                        if (life > max_life)
-                            life = max_life;
+                        heal = intelligence * 10;// determines value of heal spell
+                        cout << "You heal for " << heal << " health.";//displays heal spell
+                        life += heal;//performs heal
+                        if (life > max_life)// checks if heal exceeds maximum life
+                            life = max_life;// if heal exceeds max life, sets life to max value
                         spell_counter--;
                         ++monster_attack_counter;
                      }
@@ -417,7 +313,7 @@ int main()
    max_life += (((level + 1)/2) * 10);
    ++intelligence;
    ++spell_counter;
-
+   hero_atk_mod += 2 * level;
 
    cout << "**************************************" << endl
         << "You leveled up! You are now level " << level << "!" << endl
@@ -425,10 +321,104 @@ int main()
     }
     while (life > 0);
 
-
     system("PAUSE>NUL");
 
     return 0;
+}
+void buff()
+{
+cout << "******************************************\n"
+     << "You are endowed with the POWER OF THE GODS!"
+     << "\n****************************************\n";
+     return;
+}
 
+string item_drop1 () // A function that generates drops from low level enemies.
+{
+    int drop_roll,
+        drop;
+    const int ARRAY_ITEM_SIZE = 6;
+    string array_item [ARRAY_ITEM_SIZE] = {"Nothing", "Lifeblood", "Corrupted Soul", // Populates an array of possible items
+     "Knuckle Spikes", "Glittering Emerald Ring", "Gilded Behemoth Spine"};
 
+    srand(time(NULL));
+    drop_roll = (rand() % 105 + 1); // assigns proabilities for each potential item to be dropped and rolls for that item.
+
+    if (drop_roll <= 70)
+        {
+            array_item [0];
+            drop = 0;
+        }
+    else if (drop_roll <= 85)
+        {
+            array_item [1];
+            drop = 1;
+        }
+    else if (drop_roll <= 94)
+        {
+            array_item [2];
+            drop = 2;
+        }
+    else if (drop_roll <= 100)
+        {
+            array_item [3];
+            drop = 3;
+        }
+    else if (drop_roll <= 103)
+        {
+            array_item [4];
+            drop = 4;
+        }
+    else if (drop_roll <= 105)
+        {
+            array_item [5];
+            drop = 5;
+        }
+
+    return array_item [drop];
+}
+
+string item_drop2 ()// A function that determines items to be dropped for high level enemies.
+{
+    int drop_roll, //Variable of the integer value rolled.
+        drop; // variable of which item gets called from the array
+    const int ARRAY_ITEM_SIZE = 6;
+    string array_item [ARRAY_ITEM_SIZE] = {"Nothing", "Lifeblood", "Corrupted Soul",// Populates an array of potential items
+     "Knuckle Spikes", "Glittering Emerald Ring", "Gilded Behemoth Spine"};
+
+    srand(time(NULL));
+    drop_roll = (rand() % 105 + 1);
+
+    if (drop_roll <= 25)
+        {
+            array_item [0];
+            drop = 0;
+        }
+    else if (drop_roll <= 50)
+        {
+            array_item [1];
+            drop = 1;
+        }
+    else if (drop_roll <= 75)
+        {
+            array_item [2];
+            drop = 2;
+        }
+    else if (drop_roll <= 90)
+        {
+            array_item [3];
+            drop = 3;
+        }
+    else if (drop_roll <= 100)
+        {
+            array_item [4];
+            drop = 4;
+        }
+    else if (drop_roll <= 105)
+        {
+            array_item [5];
+            drop = 5;
+        }
+
+    return array_item [drop];
 }
